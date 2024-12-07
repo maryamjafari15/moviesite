6fbdfd18287ffea08da2de5dc26e9e66b4bc0ad5
maryam jafari <maryamjafari12175@gmail.com>
2024-12-07 15:55:36 +0330

    -v3

--- src/components/ProgressChart/ProgressChart.jsx
+++ src/components/ProgressChart/ProgressChart.jsx
@@ -1,24 +1,21 @@
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
-export function ProgressChart(props){
-    const {percentage , text} = props
-    return (
-       <>
-          <CircularProgressbar
-           className="w-11 "
-            value={percentage}
-            text={text}
-            styles={buildStyles({
-              textColor: "#ffffff",
-              pathColor: percentage > 50 ? "green" : "red",
-              trailColor: "#d6d6d6",
-              
-            
-            })}
-          />
-          
-          </>
-      );
-
-} 
+export function ProgressChart(props) {
+  const { percentage, text } = props;
+  return (
+    <>
+      <div style={{ width: '50px', height: '50px' }}>
+        <CircularProgressbar
+          value={percentage}
+          text={text}
+          styles={buildStyles({
+            textColor: "#ffffff",
+            pathColor: percentage > 50 ? "green" : "red",
+            trailColor: "#d6d6d6",
+          })}
+        />
+      </div>
+    </>
+  );
+}
